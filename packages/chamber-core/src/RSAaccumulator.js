27897b71fcfef7d383718aa9a128a89f295afdb5
SHUHEI HIYA <syuhei176@gmail.com>
2018-12-11 16:00:02 +0900

    Modify RSA accumulator

--- packages/chamber-core/src/RSAaccumulator.js
+++ packages/chamber-core/src/RSAaccumulator.js
@@ -1,111 +1,110 @@
-/*  This code is written to show how RSAaccumulator runs.
-    Some parts are simplified very much. */ 
-class BigNumber{    //simplified. be not big yet.
-    constructor(value){
-        this.value = value;
-    }
+const BigNumber = require('bignumber.js');
+const utils = require('ethereumjs-util');
 
-    powInMod(y,z){  //calculate (value**y) % z for int y, BigNumber z.
-        let x = 1;
-        var a = this.value;
-        while(y > 0){
-            if(y%2 == 1){
-                x = (x*a) % z.value;
-                y--;
-            }
-            y /= 2;
-            a= (a*a) % z.value;
-        }
-        return new BigNumber(x);
-    }
+/**
+ * @description This code is written to show how RSAaccumulator runs.
+ * Some parts are simplified very much.
+ */
 
-    mult(x){
-        return new BigNumber(this.value * x.value);
+/**
+ * @description calculate (value**y) % z for int y, BigNumber z.
+ * @param {*} y 
+ * @param {*} z 
+ */
+function powInMod(a, y, z) {
+  let x = BigNumber(1);
+  while(y.comparedTo(0) == 1) {
+    if(y.mod(2).toNumber() === 1) {
+      x = (x.times(a)).mod(z);
+      y = y.minus(1);
     }
-
-    mod(x){
-        return new BigNumber(this.value % x.value);
-    }
-
-    equal(x){
-        return this.value == x.value;
-    }
+    y = y.div(2);
+    a = a.times(a).mod(z);
+  }
+  return x;
 }
 
-N=new BigNumber(134);//should be big. And the prime factors of N should be unknown to anyone.
+// should be big. And the prime factors of N should be unknown to anyone.
+const N = new BigNumber(131071);
 
-class RSAaccumulator{
-    static updateAccumulator(accumulator, value){
-        return accumulator.powInMod(value,N);
+class RSAaccumulator {
+  
+  static updateAccumulator(accumulator, value) {
+    return powInMod(accumulator, value, N);
+  }
+  
+  static hash(value) {
+    const hash = '0x' + utils.sha256(value.toString()).toString('hex')
+    return new BigNumber(hash);
+  }
+  
+  static getProof(setValues, value, generator, accumulator){
+    // calcute (q,r,x). it satisfies q**B * g**(vr)* g**x == A
+    let h = powInMod(generator, value, N);
+    let B = RSAaccumulator.hash(h.plus(accumulator));
+    let q = BigNumber(1);
+    let r = BigNumber(0);
+    let x = BigNumber(1);
+    for(let i = 0; i < setValues.length; i++){
+      let x2 = (x.times(setValues[i])).mod(value);
+      let temp = ((x.times(setValues[i])).div(value))
+                  .plus(r.times(setValues[i]))
+                  .integerValue(BigNumber.ROUND_FLOOR);
+      let r2 = temp.mod(B);
+      let q2 = powInMod(BigNumber(1), setValues[i], N)
+                .times(powInMod(h, (temp.div(B)).integerValue(BigNumber.ROUND_FLOOR), N))
+                .mod(N);
+      x = x2;
+      r = r2;
+      q = q2;
     }
+    console.log('get proof', q, r, x);
+    return new RSAaccumulatorProof(value, q, r, x);
+  }
 
-    static hash(value){//should be hash.
-        return 255;
+  static checkNonInclusionProof(generator, accumulator, proof) {
+    let h = powInMod(generator, proof.value, N);
+    let B = RSAaccumulator.hash(h.plus(accumulator));
+    if(proof.x.comparedTo(0) <= 0 || proof.value.comparedTo(proof.x) <= 0){
+      return false;
     }
+    if(proof.r.comparedTo(0) <= 0 || B.comparedTo(proof.r) <= 0){
+      return false;
+    }
+    if(powInMod(proof.q, B, N).times(powInMod(generator, proof.r.times(proof.value).plus(proof.x), N)).mod(N).eq(accumulator)) {
+      return true;
+    }else{
+      return false;
+    }
+  }
 
-    static getProof(setValues, value, generator, accumulator){
-        // calcute (q,r,x). it satisfies q**B * g**(vr)* g**x == A
-        let h = generator.powInMod(value,N);
-        let B = RSAaccumulator.hash(h+accumulator.value);
-        let q = new BigNumber(1);
-        let r = 0;
-        let x = 1;
-        for(let i=0;i<setValues.length;i++){
-            let x2 = (x * setValues[i]) % value;
-            let temp = Math.floor((x * setValues[i]) / value) + (r * setValues[i]);
-            let r2 = temp % B;
-            let q2 = q.powInMod(setValues[i],N).mult(h.powInMod(Math.floor(temp/B),N)).mod(N);
-            x=x2;
-            r=r2;
-            q=q2;
-        }
-        return new RSAaccumulatorProof(value,q,r,x);
+  static checkInclusionProof(generator, accumulator, proof){
+    let h = powInMod(generator, proof.value, N);
+    let B = RSAaccumulator.hash(h.plus(accumulator));
+    if(!proof.x.eq(0)){
+      return false;
     }
-
-    static checkNonInclusionProof(generator, accumulator, proof){
-        let h = generator.powInMod(proof.value,N);
-        let B = RSAaccumulator.hash(h+accumulator.value);
-        if(proof.x <= 0 || proof.value <= proof.x){return false;}
-        if(proof.r <= 0 || B <= proof.r){return false;}
-        if(proof.q.powInMod(B,N).mult(generator.powInMod(proof.r*proof.value+proof.x,N)).mod(N).equal(accumulator)){
-            return true;
-        }else{
-            return false;
-        }
+    if(proof.r.comparedTo(0) <= 0 || B.comparedTo(proof.r) <= 0){
+      return false;
     }
-
-    static checkInclusionProof(generator, accumulator, proof){
-        let h = generator.powInMod(proof.value,N);
-        let B = RSAaccumulator.hash(h+accumulator.value);
-        if(proof.x !== 0){return false;}
-        if(proof.r <= 0 || B <= proof.r){return false;}
-        if(proof.q.powInMod(B,N).mult(generator.powInMod(proof.r*proof.value+proof.x,N)).mod(N).equal(accumulator)){
-            return true;
-        }else{
-            return false;
-        }
+    if(powInMod(proof.q, B, N).times(powInMod(generator, proof.r.times(proof.value).plus(proof.x), N)).mod(N).eq(accumulator)){
+        return true;
+    }else{
+        return false;
     }
+  }
 }
 
 class RSAaccumulatorProof{
-    constructor(value,q,r,x){
-        this.value=value;
-        this.q=q;
-        this.r=r;
-        this.x=x;
-    }
+  constructor(value,q,r,x){
+    this.value=value;
+    this.q=q;
+    this.r=r;
+    this.x=x;
+  }
 }
-//test
-coins=[29,31,37];
 
-generator=new BigNumber(3);
-accumulator=generator;
-for(i=0;i<coins.length;i++){
-accumulator=RSAaccumulator.updateAccumulator(accumulator,coins[i]);
+
+module.exports = {
+  RSAaccumulator
 }
-console.log(generator);
-console.log(accumulator);
-proof=RSAaccumulator.getProof(coins,29,generator,accumulator);
-console.log(proof);
-console.log(RSAaccumulator.checkNonInclusionProof(generator,accumulator,proof));
-console.log(RSAaccumulator.checkInclusionProof(generator,accumulator,proof));
