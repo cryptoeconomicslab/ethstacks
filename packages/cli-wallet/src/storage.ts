2b34c2ba4bd97d568e9a696f0f3931c1666b162a
SHUHEI HIYA <syuhei176@gmail.com>
2019-04-08 23:26:30 +0900

    Impl file system storage for wallet

--- packages/cli-wallet/src/storage.ts
+++ packages/cli-wallet/src/storage.ts
@@ -3,67 +3,66 @@ import * as leveldown from 'leveldown'
 
 import LevelUp = levelup.LevelUp
 import { IStorage } from '@layer2/wallet'
-import fs from 'fs'
 import path from 'path'
 
 export class FileStorage implements IStorage {
-  db: LevelUp
+  metaDb: LevelUp
+  proofDb: LevelUp
+  bhDb: LevelUp
   path: string
-  data: Map<string, string>
-  blockHeaders: Map<number, string>
   actions: Map<number, string>
 
-  constructor(path: string) {
-    this.path = path
-    const down = leveldown.default(path)
-    this.db = levelup.default(down)
-    this.data = new Map<string, string>()
-    this.blockHeaders = new Map<number, string>()
+  constructor(basePath: string) {
+    this.path = basePath
+    const meta = leveldown.default(path.join(this.path, 'meta'))
+    const proof = leveldown.default(path.join(this.path, 'proof'))
+    const bh = leveldown.default(path.join(this.path, 'bh'))
+    this.metaDb = levelup.default(meta)
+    this.proofDb = levelup.default(proof)
+    this.bhDb = levelup.default(bh)
     this.actions = new Map<number, string>()
   }
 
   async set(key: string, value: string): Promise<boolean> {
-    await this.db.put(key, value)
+    await this.metaDb.put(key, value)
     return true
   }
   async get(key: string): Promise<string> {
-    const value = await this.db.get(key)
+    const value = await this.metaDb.get(key)
     if(value) return String(value)
     else throw new Error(`key ${key} not found`)
   }
   async delete(key: string): Promise<boolean> {
-    await this.db.del(key)
+    await this.metaDb.del(key)
     return true
   }
   async addProof(key: string, blkNum: number, value: string): Promise<boolean> {
-    this.data.set(key + '.' + blkNum, value)
+    this.proofDb.put(key + '.' + blkNum, value)
     return Promise.resolve(true)
   }
-  getProof(key: string, blkNum: number): Promise<string> {
-    const value = this.data.get(key+ '.' + blkNum)
-    if(value)
-      return Promise.resolve(value)
-    else
-    return Promise.reject(new Error(`key ${key} not found`))
+  async getProof(key: string, blkNum: number): Promise<string> {
+    return await this.proofDb.get(key+ '.' + blkNum)
   }
-  addBlockHeader(blkNum: number, value: string): Promise<boolean> {
-    this.blockHeaders.set(blkNum, value)
-    return Promise.resolve(true)
+  async addBlockHeader(blkNum: number, value: string): Promise<boolean> {
+    await this.bhDb.put(blkNum, value)
+    return true
   }
-  getBlockHeader(blkNum: number): Promise<string> {
-    const value = this.blockHeaders.get(blkNum)
-    if(value)
-      return Promise.resolve(value)
-    else
-    return Promise.reject(new Error(`key ${blkNum} not found`))
+  async getBlockHeader(blkNum: number): Promise<string> {
+    return await this.bhDb.get(blkNum)
   }
-  searchBlockHeader(fromBlkNum: number, toBlkNum: number): Promise<{blkNum: number, value: string}[]> {
-    const arr: {blkNum: number, value: string}[] = []
-    this.blockHeaders.forEach((val, key) => {
-      if(key >= fromBlkNum && key < toBlkNum)
-        arr.push({blkNum: key, value: val})
+  async searchBlockHeader(fromBlkNum: number, toBlkNum: number): Promise<{blkNum: number, value: string}[]> {
+    const blkNums = []
+    for(let i = fromBlkNum;i < toBlkNum;i++) {
+      blkNums.push(i)
+    }
+    const tasks = blkNums.map(async (blkNum) => {
+      const blockHeader = await this.getBlockHeader(blkNum)
+      return {
+        blkNum: blkNum,
+        value: blockHeader
+      }
     })
-    return Promise.resolve(arr)
+    return await Promise.all(tasks)
   }
   addAction(id: string, blkNum: number, value: string): Promise<boolean> {
     this.actions.set(blkNum, value)
